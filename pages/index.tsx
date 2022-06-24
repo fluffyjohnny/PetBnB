51b463b25cb88e8891e788a92a3da004b3c44024
John Chia <john.chia0106@gmail.com>
2022-06-23 18:02:40 -0700

    create main section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,45 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import Header from "../components/Header";
+import Banner from "../components/Banner";
 
-const Home: NextPage = () => {
+export async function getStaticProps() {
+  const exploreData = await fetch("https://links.papareact.com/pyp").then(
+    (res) => res.json()
+  );
+
+  return {
+    props: {
+      exploreData
+    }
+  }
+}
+
+
+
+const Home: NextPage = ({ exploreData }) => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="">
       <Head>
         <title>PetBnB</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-        
-    <h1>hello</h1>
+      <Header />
+      <Banner />
 
+      <main className="max-w-7xl mx-auto px-8 sm:px-16">
+        <section className="pt-6">
+          <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
+          {exploreData.map(item => (
+            <h1> {item.location}</h1>
+          ))}
+        </section>
+      </main>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
+
