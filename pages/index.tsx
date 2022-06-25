f5b5b8461f572e36127f2d1ffa630122f69f5d76
John Chia <john.chia0106@gmail.com>
2022-06-25 02:51:07 -0700

    create section components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,30 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
 import Header from "../components/Header";
 import Banner from "../components/Banner";
+import SmallCard from "../components/SmallCard";
+import MediumCard from "../components/MediumCard";
+import LargeCard from "../components/LargeCard";
+import Footer from "../components/Footer";
 
 export async function getStaticProps() {
   const exploreData = await fetch("https://links.papareact.com/pyp").then(
     (res) => res.json()
   );
 
+  const cardsData = await fetch("https://links.papareact.com/zp1").then((res) =>
+    res.json()
+  );
+
   return {
     props: {
-      exploreData
-    }
-  }
+      exploreData,
+      cardsData,
+    },
+  };
 }
 
-
-
-const Home: NextPage = ({ exploreData }) => {
+const Home: NextPage = ({ exploreData, cardsData }) => {
   return (
     <div className="">
       <Head>
@@ -32,14 +38,35 @@ const Home: NextPage = ({ exploreData }) => {
       <main className="max-w-7xl mx-auto px-8 sm:px-16">
         <section className="pt-6">
           <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
-          {exploreData.map(item => (
-            <h1> {item.location}</h1>
-          ))}
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            {exploreData?.map((item, index) => (
+              <SmallCard
+                key={index}
+                img={item.img}
+                distance={item.distance}
+                location={item.location}
+              />
+            ))}
+          </div>
         </section>
+        <section>
+          <h2 className="text-4xl font-semibold py-8">Live Anywhere</h2>
+          <div className="flex space-x-3 overflow-scroll scrollbar-hide p-3 ">
+            {cardsData?.map(({ img, title }) => (
+              <MediumCard key={img} img={img} title={title} />
+            ))}
+          </div>
+        </section>
+        <LargeCard
+          img="https://links.papareact.com/4cj"
+          title="The Greatest Outdoors"
+          description="Wishlists curated by AirBnB"
+          buttonText="Get Inspired"
+        />
       </main>
+      <Footer />
     </div>
   );
 };
 
 export default Home;
-
