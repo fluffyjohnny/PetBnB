50acbeea0255448d441b1962bc39a463c8935d9b
John Chia <john.chia0106@gmail.com>
2022-06-29 20:52:39 -0700

    add mapbox key as an env variable

--- next.config.js
+++ next.config.js
@@ -4,4 +4,7 @@ module.exports = {
   images: {
     domains: ["links.papareact.com"],
   },
+  env: {
+    REACT_MAPBOX_KEY: 'pk.eyJ1Ijoiam9obnZzam9objg2IiwiYSI6ImNsNHlvbGc0bzBocDEzanBhcWk1M3hoYzEifQ.H_LVHRD9sy95aAG7O2_3Kw'
+  }
 };
