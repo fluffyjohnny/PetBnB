f1d2f067f4208df592e35d5bde35be6531289915
John Chia <john.chia0106@gmail.com>
2022-08-09 12:44:59 -0700

    change fineprint on footer

--- components/Footer.js
+++ components/Footer.js
@@ -16,29 +16,28 @@ function Footer() {
       <div className="space-y-4 text-xs text-gray-800">
         <h5 className="font-bold"> COMMUNITY </h5>
         <p>Accessibility</p>
-        <p>This is not a real site</p>
-        <p>Investors</p>
-        <p>Airbnb Plus</p>
-        <p>Airbnb Luxe</p>
+        <p>Disaster relief housing</p>
+        <p>Support refugees</p>
+        <p>Combating discrimination</p>
       </div>
 
 
       <div className="space-y-4 text-xs text-gray-800">
         <h5 className="font-bold"> HOST </h5>
-        <p>How Airbnb Works</p>
-        <p>Newsroom</p>
-        <p>Investors</p>
-        <p>Airbnb Plus</p>
-        <p>Airbnb Luxe</p>
+        <p>Become a host</p>
+        <p>Perks</p>
+        <p>Explore hosting resources</p>
+        <p>Visit our community forum</p>
+        <p>How to host responsibly</p>
       </div>
 
       <div className="space-y-4 text-xs text-gray-800">
         <h5 className="font-bold"> SUPPORT </h5>
-        <p>How Airbnb Works</p>
-        <p>Newsroom</p>
-        <p>Investors</p>
-        <p>Airbnb Plus</p>
-        <p>Airbnb Luxe</p>
+        <p>Help Center</p>
+        <p>Safty information</p>
+        <p>Cancellation options</p>
+        <p>Our COVID-19 response</p>
+        <p>Report a neighborhood concern</p>
       </div>
 
       <div></div>
